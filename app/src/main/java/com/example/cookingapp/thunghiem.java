27b85686f715f8673786ec65324c4974192d7c17
linhcter <vulinh08102001@gmail.com>
2020-11-13 22:52:31 +0700

    khởi tạo git

---
+++ app/src/main/java/com/example/cookingapp/thunghiem.java
@@ -0,0 +1,21 @@
+package com.example.cookingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class thunghiem extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_thunghiem);
+    }
+
+    // lý do k đc là do bên ngoài project e đã đồng bộ rồi, bên trong mới chứa project của e
+    // ngay từ đầu a nói tạo dự án android trc rồi hãy đồng bộ lên git
+    // là gòi em đồng bộ lại từ đầu hả a
+    // xóa git
+    // tạo repositores mới rồi úp lên alij
+
+}
